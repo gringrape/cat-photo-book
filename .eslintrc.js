1ccc940210ac02fd5062ef9a8a3ee062757dfbdb
gringrape <gringrape200@gmail.com>
2021-07-06 23:59:23 +0900

    Initial setup

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    jest: true,
+  },
+  globals: {
+    context: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {
+  },
+};
